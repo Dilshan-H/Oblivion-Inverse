bd4ba139b77f592ac03a5e0da2aed3399a9df926
SU-Local <77499497+Dilshan-H@users.noreply.github.com>
2022-04-15 17:21:08 +0530

    Initial commit

---
+++ forms.py
@@ -0,0 +1,12 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import DataRequired
+
+class GenerateTrackingLink(FlaskForm):
+    email_title = StringField('Title', validators=[DataRequired()])
+    submit = SubmitField('GENERATE')
+    
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    login = SubmitField('LOGIN')
