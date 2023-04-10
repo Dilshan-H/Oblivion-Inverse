0857f30155b38253e69ae78365cd6cf375b942de
dilshan-h <77499497+Dilshan-H@users.noreply.github.com>
2023-04-10 20:15:57 +0530

    Format code

--- app.py
+++ app.py
@@ -2,27 +2,29 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from routes import *
+from models import Users
 
-app = Flask(__name__, static_url_path='/static', static_folder='static')
+app = Flask(__name__, static_url_path="/static", static_folder="static")
 
-app.config['DEBUG'] = False
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL'].replace('postgres://', 'postgresql://')
-app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
+app.config["DEBUG"] = False
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ["DATABASE_URL"].replace(
+    "postgres://", "postgresql://"
+)
+app.config["SECRET_KEY"] = os.environ["SECRET_KEY"]
 
 db = SQLAlchemy(app)
 
 login_manager = LoginManager()
-login_manager.login_view = 'login'
+login_manager.login_view = "login"
 login_manager.init_app(app)
 
-from routes import *
-from models import Users
 
 @login_manager.user_loader
 def load_user(user_id):
     return Users.query.get(int(user_id))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=False)
